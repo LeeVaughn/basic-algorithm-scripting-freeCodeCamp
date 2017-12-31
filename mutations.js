2c81f270b66dafff907bf363fef114337e724424
Lee Vaughn <32204461+LeeVaughn@users.noreply.github.com>
2017-12-30 20:38:03 -0800

    Add files via upload
    Added solution to fCC Basic Aglorithm #12, Mutations.
    https://www.freecodecamp.org/challenges/mutations

---
+++ mutations.js
@@ -0,0 +1,25 @@
+// freeCodeCamp Basic Algorithm Scripting Challenge #12 //
+// https://www.freecodecamp.org/challenges/mutations //
+
+// Mutations //
+//Return true if the string in the first element of the array contains //
+// all of the letters of the string in the second element of the array. //
+function mutation(arr) {
+    return arr;
+}
+
+mutation(["hello", "hey"]);
+
+// Solution //
+function mutation(arr) {
+    var firstElement = arr[0].toLowerCase();
+    var secondElement = arr[1].toLowerCase();
+
+    for (var i = 0; i < secondElement.length; i++) {
+        if (firstElement.indexOf(secondElement[i]) === -1)
+            return false;
+    }
+    return true;
+}
+
+mutation(["hello", "hey"]);
