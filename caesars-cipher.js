f3386b53fdc045861e27a24d14a4eecbd026f911
Lee Vaughn <32204461+LeeVaughn@users.noreply.github.com>
2018-01-03 19:05:28 -0800

    Add files via upload
    Uploaded a solution for the 16th and final freeCodeCamp Basic Algorithm. Yay!

---
+++ caesars-cipher.js
@@ -0,0 +1,22 @@
+// freeCodeCamp Basic Algorithm Scripting Challenge #1 //
+// https://www.freecodecamp.org/challenges/caesars-cipher //
+
+// Caesars Cipher //
+// Write a function which takes a ROT13 encoded string as input and returns a decoded string. //
+// All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on. //
+function rot13(str) { // LBH QVQ VG!
+
+    return str;
+}
+
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
+
+// Solution //
+function rot13(str) { // LBH QVQ VG!
+
+    return str;
+}
+
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");  
